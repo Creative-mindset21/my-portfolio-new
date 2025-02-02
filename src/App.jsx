8171c5a1a082a07b110c7d0b5af88376fe389ecf
Creative Mindset <tobiawotoye03@gmail.com>
2025-02-02 20:34:51 +0000

    imported custom fonts and created the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
-function App() {}
+import { BrowserRouter, Route, Routes } from "react-router";
+import Nav from "./layout/Nav";
+import About from "./components/About";
+import Project from "./components/Project";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <div className="container">
+      <Nav />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="projects" element={<Project />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
 
 export default App;
