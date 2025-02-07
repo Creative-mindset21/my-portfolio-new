582eb9e181e770b0f334e2d46c6ac39a86ee149a
Creative Mindset <tobiawotoye03@gmail.com>
2025-02-07 04:03:05 +0000

    made the select tag filter the product card by any option clicked

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,37 @@ import Nav from "./layout/Nav";
 import About from "./components/About";
 import Project from "./components/Project";
 import Contact from "./components/Contact";
+import { useState } from "react";
+import { projectsData } from "./data/data";
 
 function App() {
+  const [filter, setFilter] = useState("all");
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const filteredItems =
+    filter === "all"
+      ? projectsData
+      : projectsData.filter((item) => item.category === filter);
+
   return (
     <div className="wrapper">
       <BrowserRouter>
         <Nav />
         <Routes>
           <Route path="/" element={<About />} />
-          <Route path="projects" element={<Project />} />
+          <Route
+            path="projects"
+            element={
+              <Project
+                handleFilterChange={handleFilterChange}
+                filteredItems={filteredItems}
+                filter={filter}
+              />
+            }
+          />
           <Route path="contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,7 @@
-import { projectsData } from "../data/data";
 import ProjectCard from "./ProjectCard";
+import PropTypes from "prop-types";
 
-const Project = () => {
+const Project = ({ filteredItems, handleFilterChange, filter }) => {
   return (
     <section className="bg-grey-600 lg:rounded-lg p-4">
       <div className="flex items-center justify-between gap-4">
@@ -11,25 +11,32 @@ const Project = () => {
           {/* ! ===== MOBILE PHONES ===== */}
           <label htmlFor="projectSelect"></label>
           <select
-            name=""
+            value={filter}
+            onChange={handleFilterChange}
             id="projectSelect"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 md:hidden dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           >
             <option value="all">All</option>
-            <option value="web-applications">Web Applications</option>
-            <option value="tools">Tools</option>
-            <option value="ui-design">UI Design</option>
+            <option value="Web Application">Web Applications</option>
+            <option value="Utility Application">Tools</option>
+            <option value="UI Design">UI Design</option>
           </select>
         </div>
       </div>
 
       <div className="projects">
-        {projectsData.map((project) => (
+        {filteredItems.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
       </div>
     </section>
   );
+};
+
+Project.propTypes = {
+  filteredItems: PropTypes.array,
+  handleFilterChange: PropTypes.func,
+  filter: PropTypes.string,
 };
 
 export default Project;
